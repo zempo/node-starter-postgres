3703fe80081691593d3ef98d615b3899ceca9b0d
zempo <zelenkosolomon@gmail.com>
2021-03-22 13:51:40 -0700

    update jwtStarter

--- src/middleware/jwtAuthMW.js
+++ src/middleware/jwtAuthMW.js
@@ -4,9 +4,29 @@ const requireAuth = (res, req, next) => {
     const authToken = req.get('Authorization') || "";
 
     let bearerToken;
-    if (!authToken.toLowerCase().startsWith('bearer ')) {
-        
+    if (!authToken.toLowerCase().startsWith("bearer ")) {
+        return res.status(401).json({error: {message: "Unauthorized Request. Please provide bearer token."}})
     } else {
-        
+        bearerToken = authToken.slice(7)
+    }
+    try {
+        const payload = AuthService.verifyJwt(bearerToken)
+
+        AuthService.getUserByEmail(req.app.get('db'), payload.sub)
+        .then((user) => {
+            if (!user) {
+                return res.status(401).json({error: {message: "Unauthorized Request. Please provide proper credentials"}})
+            }
+            req.user = user
+            next()
+        })
+        .catch(err => {
+            console.error(err)
+            next(err)
+        })
+    } catch (error) {
+        return res.status(401).json({error: {message: "Unauthorized Request. Please provide proper credentials."}})        
     }
 }
+
+module.exports = { requireAuth }
