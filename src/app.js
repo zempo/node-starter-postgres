a2c4ec54479b263e8de06d3d5e800f929ec78356
zempo <zelenkosolomon@gmail.com>
2021-03-23 11:16:04 -0700

    add node starter template

--- src/app.js
+++ src/app.js
@@ -8,19 +8,43 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const winston = require('winston')
 /**
- * LOCALS + CONFIG
+ * LOCAL IMPORTS
 */
+const logger = require('./middleware/logMW').logger
+const errCatch = require('./middleware/errMW')
 const { NODE_ENV } = require("./config");
 
+// Route imports
+const authRouter = require('./routes/authRouter')
+const usersRouter = require('./routes/usersRouter')
+
 //----------------------------------------------
+const app = express()
 
 /**
  * MIDDLEWARE
 */
+app.use(cors())
+const morganOption = NODE_ENV === "production" ? "tiny" : "dev"
+if (NODE_ENV !== "production") {
+    logger.add(
+        new winston.transports.Console({
+            format: winston.format.simple()
+        })
+    )
+}
+app.use(morgan(morganOption))
+app.use(helmet())
 
 /**
  * ROUTES
 */
+app.use('/api/auth', authRouter)
+app.use('/api/users', usersRouter)
 
+app.get('/', (req, res) => {
+    // load app status pg w/ variable checks
+    res.status(200).json('Welcome to your app')
+})
 
 module.exports = app
