f6cf276db73c9eca7041bfb10079f6b019595ee1
zempo <zelenkosolomon@gmail.com>
2021-03-24 16:14:27 -0700

    env vars working

--- src/app.js
+++ src/app.js
@@ -1,53 +1,129 @@
-require('dotenv').config();
 /**
  * DEPENDENCIES
-*/
-const cors = require('cors')
-const express = require('express')
-const helmet = require('helmet')
-const morgan = require('morgan')
-const winston = require('winston')
+ */
+const cors = require("cors");
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const winston = require("winston");
 /**
  * LOCAL IMPORTS
-*/
-const logger = require('./middleware/logMW').logger
-const errCatch = require('./middleware/errMW')
-const { NODE_ENV } = require("./config");
+ */
+const logger = require("./middleware/logMW").logger;
+const errCatch = require("./middleware/errMW");
+const { NODE_ENV, WORKING, PORT } = require("./config");
 
 // Route imports
-const authRouter = require('./routes/authRouter')
-const usersRouter = require('./routes/usersRouter')
+const authRouter = require("./routes/authRouter");
+const usersRouter = require("./routes/usersRouter");
 
 //----------------------------------------------
-const app = express()
+const app = express();
 
 /**
  * MIDDLEWARE
-*/
-app.use(cors())
-const morganOption = NODE_ENV === "production" ? "tiny" : "dev"
+ */
+app.use(cors());
+const morganOption = NODE_ENV === "production" ? "tiny" : "dev";
 if (NODE_ENV !== "production") {
-    logger.add(
-        new winston.transports.Console({
-            format: winston.format.simple()
-        })
-    )
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
 }
-app.use(morgan(morganOption))
-app.use(helmet())
+app.use(morgan(morganOption));
+app.use(helmet());
 
 /**
  * ROUTES
-*/
-app.use('/api/auth', authRouter)
-app.use('/api/users', usersRouter)
+ */
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
+  let statusPg = `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Server Status</title>
+    </head>
+    <body>
+      <style>
+        @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300&family=Roboto&display=swap");
+  
+        body {
+          background: #f0efef;
+          margin: 0;
+          padding: 10rem 5rem;
+        }
+  
+        h1 {
+          text-align: center;
+          color: #8f6d19;
+          font-family: "Raleway", sans-serif;
+          font-size: calc(1.7rem + 1vw);
+        }
+  
+        main {
+          max-width: 1200px;
+          margin: auto;
+        }
+  
+        h2 {
+          color: #4d4d4d;
+          font-family: "Raleway", sans-serif;
+          font-size: calc(1rem + 0.5vw);
+          margin-top: 5rem;
+          line-height: 1;
+        }
+  
+        ul {
+          list-style: none;
+        }
+  
+        li {
+          font-family: "Roboto", sans-serif;
+          font-size: calc(1rem + 0.25vw);
+          text-transform: uppercase;
+          line-height: 2;
+        }
+  
+        .on {
+          color: rgb(36, 150, 21);
+        }
+  
+        .off {
+          color: rgb(233, 32, 75);
+        }
+      </style>
+      <h1>Server for ''Your App''</h1>
+      <main>
+        <h2>Debug</h2>
+        <hr />
+        <ul>
+          <li>
+            <b>Env Vars:</b>
+            <span class="${
+              WORKING === "online" ? "on" : "off"
+            }"> ${WORKING} </span>
+          </li>
+        </ul>
+        <h2>Routes</h2>
+        <hr />
+        <ul>
+          <li>
+            <a href="http://localhost:${PORT}">Home</a>
+          </li>
+        </ul>
+      </main>
+    </body>
+  </html>`;
 
-    let statusPg = ``
+  // load app status pg w/ variable checks
+  res.status(200).send(statusPg);
+});
 
-    // load app status pg w/ variable checks
-    res.status(200).send(statusPg)
-})
-
-module.exports = app
+module.exports = app;
--- src/config.js
+++ src/config.js
@@ -1,14 +1,20 @@
+const dotenv = require("dotenv").config();
+
 module.exports = {
-    PORT: process.env.PORT || 8000,
-    NODE_ENV: process.env.NODE_ENV || "development",
-    DB_URL: process.env.DB_URL,
-    JWT_SECRET: process.env.JWT_SECRET || "test-jwt-secret",
-    JWT_EXPIRY: process.env.JWT_EXPIRY || "180d",
-    APIS: {
-        SOME_API_KEY: process.env.SOME_API_KEY || "loremipsum202020202"
-    },
-    REGEX: {
-        VALID_USERNAME: /(^[A-Za-z0-9\-\_]+$)/,
-        VALID_PWD: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
-    }
-}
+  // If last .env var can be read, env variables are online
+  //.................
+  WORKING: process.env.WORKING || "offline",
+  //.................
+  PORT: process.env.PORT || 8000,
+  NODE_ENV: process.env.NODE_ENV || "development",
+  DB_URL: process.env.DB_URL,
+  JWT_SECRET: process.env.JWT_SECRET || "test-jwt-secret",
+  JWT_EXPIRY: process.env.JWT_EXPIRY || "180d",
+  APIS: {
+    SOME_API_KEY: process.env.SOME_API_KEY || "loremipsum202020202",
+  },
+  REGEX: {
+    VALID_USERNAME: /(^[A-Za-z0-9\-\_]+$)/,
+    VALID_PWD: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/,
+  },
+};
